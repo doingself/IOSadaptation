162867ec408569ee2d700c9251c2f2de50c4600d
syc <doingself@163.com>
2018-03-14 17:11:57 +0800

    init project

---
+++ IOS11Demo/IOS11Demo/MeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MeViewController.swift
+//  IOS11Demo
+//
+//  Created by 623971951 on 2018/3/14.
+//  Copyright © 2018年 syc. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
