97a878471c061d5f30595f4a3e5868b1d15affbe
史彦超 <daviondk@163.com>
2020-09-27 09:42:42 +0800

    init SceneDelegate

---
+++ SceneDelegateDemo/SceneDelegateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SceneDelegateDemo
+//
+//  Created by syc-ios on 2020/9/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
